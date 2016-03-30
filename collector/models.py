76e61ecbb0690b36d671a72d05694a8947a69040
Paul Logston <code@logston.me>
2016-03-30 00:45:01 -0400

    Finish first draft of collector models

--- collector/models.py
+++ collector/models.py
@@ -2,25 +2,78 @@ from django.db import models
 
 
 class Share(models.Model):
-    company_name = models.CharField(max_length=128)
+    company_name = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+    )
+
     symbol = models.CharField(
         max_length=16,
         db_index=True,
     )
-    start = models.DateField()
-    end = models.DateField()
-    industry = models.CharField(max_length=128)
-    sector = models.CharField(max_length=128)
+
+    start = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    end = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    industry = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+    )
+
+    sector = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.symbol
 
 
 class Quote(models.Model):
     share = models.ForeignKey('collector.Share')
-    date = models.DateField()
-    open = models.FloatField()
-    close = models.FloatField()
-    adj_close = models.FloatField()
-    low = models.FloatField()
-    high = models.FloatField()
-    volume = models.IntegerField()
 
+    date = models.DateField()
+
+    open = models.FloatField(
+        null=True,
+        blank=True,
+    )
+
+    close = models.FloatField(
+        null=True,
+        blank=True,
+    )
+
+    adj_close = models.FloatField(
+        null=True,
+        blank=True,
+    )
+
+    low = models.FloatField(
+        null=True,
+        blank=True,
+    )
+
+    high = models.FloatField(
+        null=True,
+        blank=True,
+    )
+
+    volume = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return '{} / {}'.format(self.share.symbol, self.date)
 
